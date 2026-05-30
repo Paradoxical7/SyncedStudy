9538a934f2b430706222a056bc2c0210676af9b4
Nahin <nn389489@gmail.com>
2026-05-30 15:02:39 -0400

    feat: add Vercel analytics

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+import { Analytics } from "@vercel/analytics/react";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
